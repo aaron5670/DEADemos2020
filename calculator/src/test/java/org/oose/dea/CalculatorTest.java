17f1d8b3d88bc60426960e20fc06e56e2a510c27
Aaron van den Berg <a.vdberg98@gmail.com>
2020-02-05 10:36:30 +0100

    Add calculate methods

--- calculator/src/test/java/org/oose/dea/CalculatorTest.java
+++ calculator/src/test/java/org/oose/dea/CalculatorTest.java
@@ -6,9 +6,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     @Test
-    public void testAdd(){
-        Calculator calculator = new Calculator ();
-        assertEquals(4,calculator.add(2,2));
-        assertEquals(100,calculator.add(25,75));
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2, 2));
+        assertEquals(100, calculator.add(25, 75));
+    }
+
+    @Test
+    public void testMinus() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.minus(4, 2));
+        assertEquals(100, calculator.minus(125, 25));
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.divide(10, 5));
+        assertEquals(10, calculator.divide(100, 10));
+    }
+
+    @Test
+    public void testTimes() {
+        Calculator calculator = new Calculator();
+        assertEquals(250, calculator.times(50, 5));
+        assertEquals(20, calculator.times(2, 10));
     }
 }
